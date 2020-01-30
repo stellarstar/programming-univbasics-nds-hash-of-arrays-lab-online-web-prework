bc25b489c2287503cbffb5bfc977a4ff13519b8f
victor tim <technicalpro@securemedia.ch>
2020-01-30 07:16:17 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,12 +1,25 @@
 BASE_HOA ={
+<<<<<<< HEAD
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
 def add_character(show, name)
 BASE_HOA[show] << name 
+=======
+  :chipmunks => ["Alvin", "Simon", "Theodore", "Dave"],
+  :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)", "Snarf"],
+  :jetsons => ["George", "Jane", "Judy", "Elroy", "Astro", "Snarf"]
+}
+#def add_character(show, name)
+#BASE_HOA 
+>>>>>>> e6a2d83e670219681185eddc2a5684f8bbca418d
 #:chipmunks => {"Alvin", "Simon", "Theodore", "Dave"},
 #:third_earthers => {"Lion-O", "Cheetara", "Mumm-Ra (the ever-living)", "Snarf"},
 #:jetsons => {"George", "Jane", "Judy", "Elroy", "Astro", "Snarf"}
 
+<<<<<<< HEAD
 end
+=======
+#end
+>>>>>>> e6a2d83e670219681185eddc2a5684f8bbca418d
